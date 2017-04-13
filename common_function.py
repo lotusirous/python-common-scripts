65e1b8b728ad76fdbcd7df0b70351598c1f57aff
Nguyen Trong Kha <nguyentrongkha92@gmail.com>
2017-04-13 12:53:53 +0900

    Create common_function.py

---
+++ common_function.py
@@ -0,0 +1,28 @@
+import os
+from subprocess import check_output, STDOUT, CalledProcessError
+import shutil
+import hashlib
+
+def _execute_shell(cmd):
+    status = False
+    output = None
+    try:
+        output = check_output(cmd, stderr=STDOUT, shell=True, timeout=3, universal_newlines=True)
+        status = True
+    except CalledProcessError as exc:
+        print("Status : FAIL", exc.returncode, exc.output)
+
+    return status, output
+
+
+def hash_file(loc):
+    return hashlib.sha256(open(loc, 'rb').read()).hexdigest()
+
+def traverse(loc):
+    for dirpath, dirnames, filenames in os.walk(loc):
+        for basename in filenames:
+            yield basename, os.path.join(dirpath, basename)
+
+
+def copy_file(src, dst):
+    shutil.copy2(src, dst)
