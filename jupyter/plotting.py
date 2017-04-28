e12e91c274f4d9ac834b6274b6461b6893f78a98
Nguyen Trong Kha <nguyentrongkha92@gmail.com>
2017-04-28 14:05:24 +0900

    Create plotting.py

---
+++ jupyter/plotting.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from IPython.display import display, Image
+%matplotlib inline
+
+
+
+
+# display image in Jupyter
+imgpath = "/home/trongkha/sample.png"
+display(Image(filename=imgpath))
