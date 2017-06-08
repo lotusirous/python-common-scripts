75a7ad8f497df7b9ae2178df1f44695644573331
Nguyen Trong Kha <nguyentrongkha92@gmail.com>
2017-06-08 10:51:30 +0900

    Create notebook-templates.py

--- notebook-templates.py
+++ notebook-templates.py
@@ -1,5 +1,18 @@
 %matplotlib inline
 
+# ploting
 import matplotlib
-import numpy as np
 import matplotlib.pyplot as plt
+
+# 
+import numpy as np
+import pandas as pd
+
+# Keras library
+
+from keras.models import Sequential
+from keras.layers import Input, Convolution2D, MaxPooling2D, Dense, Dropout, Flatten
+from keras.utils import np_utils # utilities for one-hot encoding of ground truth values
+
+# fix random seed for reproducibility
+np.random.seed(7)
